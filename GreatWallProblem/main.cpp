6ec5c787196b6bd09971d39d3726d22b8426da4c
Hannah <N1039942@ntu.ac.uk>
2024-10-12 21:39:09 +0100

    Removing BinarySearchTree - can revert later

--- GreatWallProblem/main.cpp
+++ GreatWallProblem/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include "BinarySearchTree.h"
 
 using namespace std;
 
 int main() {
-	BinarySearchTree bst;
-    bst.insert(10, "ten");
-    bst.insert(5, "five");
-    bst.insert(15, "fifteen");
-
-    bst.displayTree();
+    cout << "Hello, World!" << endl;
+    return 0;
 }
