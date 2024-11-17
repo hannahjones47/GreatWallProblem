8e4877d6fc421ea25deba70ae20b4d84fa79aa14
Hannah <N1039942@ntu.ac.uk>
2024-11-17 22:50:43 +0000

    references

--- GreatWallProblem/include/SLLDeque.h
+++ GreatWallProblem/include/SLLDeque.h
@@ -9,6 +9,11 @@ struct Node {
   Node(const string& text) : data(text), next(nullptr) {}
 };
 
+// This class is adapted from the DLQueue implementation created in-class, found in the SOFT37002 module repository.
+// Available at: https://olympus.ntu.ac.uk/CMP3RODRIN/ASE-DA/tree/main/S3-4_LinkedLists/DLQueue [Accessed 17 November 2024]
+// Original author: Module Leader (Nuno Rodrigues Amálio)
+// Key modifications: changed to singly linked list deque, replaced integer data with strings, removed bidirectional pointers, 
+//                    added push_front/push_back, display and accessors for head/tail to suit the Great Wall Problem.
 class SLLDeque {
   public:
     SLLDeque();                   
